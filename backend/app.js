291b1a7a8f00bc78dc2f550be604acf94078063d
Jérémy <jeremy.letisserand@gmail.com>
2022-10-26 17:25:37 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+	.connect(
+		"mongodb+srv://Jerem:8NgG758YUsqc@generalcluster.r8fpobw.mongodb.net/?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const stuffRoutes = require("./routes/stuff.js");
+const userRoutes = require("./routes/user.js");
+
+const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
