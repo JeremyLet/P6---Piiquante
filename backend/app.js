372121ecd25c05430a50da53077151ca6e16f941
Jérémy <jeremy.letisserand@gmail.com>
2022-11-10 14:53:41 +0100

    <fix>mini fixes for clean code and ending data structure

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 50, // 50 req max per window (15 minutes)
+	max: 100, // 50 req max per window (15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
